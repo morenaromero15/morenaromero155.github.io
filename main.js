ef66e9c83e3c6053ae6d5ae24e031ac3622cd46c
morenaromero15 <147270550+morenaromero15@users.noreply.github.com>
2023-11-11 11:41:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,35 @@ function cambiarColorFondo() {
     let nuevoColor = '#7adceb';
     document.body.style.backgroundColor = nuevoColor;
 }
+
+function cambiarColorFondo() {
+    let nuevoColor = '#7adceb';
+    document.body.style.backgroundColor = nuevoColor;
+}
+const form = document.getElementById("myForm")
+
+function validateEmail(email) {
+    const regex = /^[^@]+[^|s@]+|.[^|s@]{2,7}$/
+    return regex.test(email)
+}
+
+function validateForm() {
+    const input = document.getElementById("email")
+    const inputValue = input.value
+
+    if (!validateEmail(inputValue)) {
+        alert("por favor ingrese un correo electronico valido")
+    } else {
+        alert("correo electronico enviado correctamente")
+    }
+}
+
+form.addEventListener("submit", function (lala) {
+    lala.preventDefault()
+    validateForm();
+})
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function () {
+        document.querySelector(".nav-links").
+            classList.toggle("nav-links-responsive")
+    })
